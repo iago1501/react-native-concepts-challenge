70cf2af38f617739b33f0554a9fc139ba719632e
Iago Espinoza <iago1501@gmail.com>
2020-06-21 19:01:58 -0300

    :sparkles: Adicionando algumas features novas e fazendo modificações no app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   FlatList,
   Text,
   StatusBar,
+  ScrollView,
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
@@ -20,17 +21,37 @@ export default function App() {
     });
   }, []);
 
-  async function handleLikeRepository(id) {
-    const response = await api.post(`repositories/${id}/like`);
+  async function handleLikeRepository(id, type) {
+    const response = await api.post(`repositories/${id}/like`, { type: type });
 
-    const att_repositorie = response.data;
+    const attRepositorie = response.data;
 
-    const att_repositories = repositories.map((repositorie) =>
-      repositorie.id === id ? att_repositorie : repositorie
+    const attRepositories = repositories.map((repositorie) =>
+      repositorie.id === id ? attRepositorie : repositorie
     );
 
-    setRespositories(att_repositories);
+    setRespositories(attRepositories);
   }
+
+  async function handleAddRepository() {
+    const response = await api.post("repositories", {
+      title: `Repositório ${repositories.length + 1}`,
+      url: "http://github.com/iago1501",
+      techs: ["Node.js", "Express", "React", "React Native"],
+    });
+
+    const newRepositorie = response.data;
+
+    setRespositories([...repositories, newRepositorie]);
+  }
+
+  const handleRemoveRepository = async (id) => {
+    api.delete(`repositories/${id}`);
+    const attRepositories = repositories.filter(
+      (repositorie) => repositorie.id !== id
+    );
+    setRespositories(attRepositories);
+  };
 
   return (
     <>
@@ -41,15 +62,27 @@ export default function App() {
           keyExtractor={(repositorie) => repositorie.id}
           renderItem={({ item: { id, title, techs, likes } }) => (
             <View style={styles.repositoryContainer}>
-              <Text style={styles.repository}>{title}</Text>
+              <View style={styles.header}>
+                <Text style={styles.repository}>{title}</Text>
+                <TouchableOpacity
+                  onPress={() => handleRemoveRepository(id)}
+                  style={styles.remove}
+                >
+                  <Text>&#10008;</Text>
+                </TouchableOpacity>
+              </View>
 
-              <View style={styles.techsContainer}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                style={styles.techsContainer}
+              >
                 {techs.map((tech) => (
                   <Text key={tech} style={styles.tech}>
                     {tech}
                   </Text>
                 ))}
-              </View>
+              </ScrollView>
 
               <View style={styles.likesContainer}>
                 <Text
@@ -57,21 +90,40 @@ export default function App() {
                   // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
                   testID={`repository-likes-${id}`}
                 >
-                  {likes} curtidas
+                  {likes} {`curtida${likes === 1 ? '' : 's'}`}
                 </Text>
+                <TouchableOpacity
+                  style={styles.likeButton}
+                  onPress={() => handleLikeRepository(id, "like")}
+                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                  testID={`like-button-${id}`}
+                >
+                  <Text style={styles.likeButtonText}>Curtir &#128526;</Text>
+                </TouchableOpacity>
+                {likes > 0 && (
+                  <TouchableOpacity
+                    style={styles.likeButton}
+                    onPress={() => handleLikeRepository(id, "unlike")}
+                    // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                    testID={`unlike-button-${id}`}
+                  >
+                    <Text style={styles.likeButtonText}>
+                      Descurtir &#128543;
+                    </Text>
+                  </TouchableOpacity>
+                )}
               </View>
-
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => handleLikeRepository(id)}
-                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-                testID={`like-button-${id}`}
-              >
-                <Text style={styles.buttonText}>Curtir</Text>
-              </TouchableOpacity>
             </View>
           )}
         />
+        <TouchableOpacity
+          style={styles.addbutton}
+          onPress={handleAddRepository}
+          // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+          testID={"add-button"}
+        >
+          <Text style={styles.addbuttonText}>Adicionar Repositório</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -81,6 +133,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#7159c1",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  remove: {
+    padding: 5,
+    height: 30,
+    borderColor: "#000",
+    borderStyle: "solid",
+    borderWidth: 0.5,
   },
   repositoryContainer: {
     marginBottom: 15,
@@ -108,22 +172,38 @@ const styles = StyleSheet.create({
   likesContainer: {
     marginTop: 15,
     flexDirection: "row",
-    justifyContent: "space-between",
+    alignItems: "center",
   },
   likeText: {
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
   },
-  button: {
-    marginTop: 10,
+  likeButton: {
+    marginTop: 2,
   },
-  buttonText: {
+  likeButtonText: {
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
-    color: "#fff",
-    backgroundColor: "#7159c1",
-    padding: 15,
+    color: "#000",
+    backgroundColor: "transparent",
+    padding: 8,
+    borderColor: "#f4cd06",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 2,
+  },
+  addbutton: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  addbuttonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#000",
+    backgroundColor: "#f4cd06",
+    padding: 10,
+    margin: 10,
   },
 });
